b5da36fa12435365204f3c5d97c3d7f30fedd87e
Jesús S.P <jesusdeserdio@gmail.com>
2020-09-23 22:17:10 +0200

    Agregada rutas

--- src/componets/App.js
+++ src/componets/App.js
@@ -2,12 +2,20 @@ import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 
 import Inicio from '../pages/Inicio'
+import Gestion from '../pages/Gestion'
+import Historial from '../pages/Historial'
+import About from '../pages/About'
+import Contacto from '../pages/Contacto'
 
 
 function App(){
     return(
         <BrowserRouter>
             <Route exact path="/" component={ Inicio }/>
+            <Route exact path="/gestion" component={ Gestion }/>
+            <Route exact path="/historial" component={ Historial }/>
+            <Route exact path="/about" component={ About }/>
+            <Route exact path="/contacto" component={ Contacto }/>
             
         </BrowserRouter>
     )
